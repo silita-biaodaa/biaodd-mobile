ab26cae465398e47bfb47518e6e9ba744eea9bd8
lidong <642540749@qq.com>
2019-09-18 15:45:25 +0800

    1545

--- biaodd_mobile/src/pages/user/bing.vue
+++ biaodd_mobile/src/pages/user/bing.vue
@@ -186,14 +186,13 @@ export default {
                    window.location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx393124fdad606b1d&redirect_uri=http%3A%2F%2Fpre-mobile.biaodaa.com%2F%23%2Fenroll&response_type=code&scope=snsapi_base&state=CD-IMIS&connect_redirect=1#wechat_redirect'
                 } else {
                    that.isShow = true
-                   that.hint = res.msg
-                   console.log(res);
-                   
+                   that.hint = res.data.msg
                     setTimeout(() => {
                       that.isShow = false;
                     }, 2000);
+                    window.location.href ='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx393124fdad606b1d&redirect_uri=http%3A%2F%2Fpre-mobile.biaodaa.com%2F%23%2Fbinging&response_type=code&scope=snsapi_base&state=CD-IMIS&connect_redirect=1#wechat_redirect' 
                 }
-                  // console.log(res);
+                  console.log(res);
               })
         },
         getCod() {
